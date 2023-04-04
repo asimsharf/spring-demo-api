27b111aa7517038ee933b63efc9123388c133213
asimsharf <asimsharf@gmail.com>
2023-04-04 23:40:26 +0300

    Add Employee Repository extends JpaRepository & Refactor Code...

--- src/main/java/com/sudagoarth/demo/service/EmployeeServiceImpl.java
+++ src/main/java/com/sudagoarth/demo/service/EmployeeServiceImpl.java
@@ -1,39 +1,44 @@
 package com.sudagoarth.demo.service;
 
-import com.sudagoarth.demo.dao.EmployeeDAO;
+import com.sudagoarth.demo.dao.EmployeeRepository;
 import com.sudagoarth.demo.entity.Employee;
-import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService{
-    private final EmployeeDAO employeeDAO;
+    private final EmployeeRepository employeeRepository;
 
-    public EmployeeServiceImpl(EmployeeDAO employeeDAO) {
-        this.employeeDAO = employeeDAO;
+    public EmployeeServiceImpl(EmployeeRepository theEmployeeRepository) {
+        this.employeeRepository = theEmployeeRepository;
     }
     @Override
     public List<Employee> findAll() {
-        return employeeDAO.findAll();
+        return employeeRepository.findAll();
     }
 
     @Override
     public Employee findById(Integer id) {
-        return employeeDAO.findById(id);
+        Optional<Employee> result = employeeRepository.findById(id);
+        Employee theEmployee = null;
+        if (result.isPresent()) {
+            theEmployee = result.get();
+        } else {
+            throw new RuntimeException("Did not find employee id - " + id);
+        }
+        return theEmployee;
     }
 
-    @Transactional
     @Override
     public Employee save(Employee theEmployee) {
-        return  employeeDAO.save(theEmployee);
+        return  employeeRepository.save(theEmployee);
     }
 
-    @Transactional
     @Override
     public void deleteById(Integer theID) {
-        employeeDAO.deleteById(theID);
+        employeeRepository.deleteById(theID);
     }
 
 }
