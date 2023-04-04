6681436c1b903330100fedec29658a50910a4253
asimsharf <asimsharf@gmail.com>
2023-04-05 02:17:57 +0300

    Spring data rest

--- src/main/java/com/sudagoarth/demo/dao/EmployeeRepository.java
+++ src/main/java/com/sudagoarth/demo/dao/EmployeeRepository.java
@@ -3,6 +3,9 @@ package com.sudagoarth.demo.dao;
 import com.sudagoarth.demo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//@RepositoryRestResource(path = "employees")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    // that's it ... no need to write any code LOL!
 
 }
