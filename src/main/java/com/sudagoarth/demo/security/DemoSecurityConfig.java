0d78490257bdb52dbba736d0884a3c3c4eee5fcf
asimsharf <asimsharf@gmail.com>
2023-04-06 01:12:00 +0300

    security plaintext, bcrypt, bcrypt-custom-table

--- src/main/java/com/sudagoarth/demo/security/DemoSecurityConfig.java
+++ src/main/java/com/sudagoarth/demo/security/DemoSecurityConfig.java
@@ -8,26 +8,18 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
 
     @Bean
-    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password("{noop}password")
-                .roles("ADMIN", "MANAGER", "EMPLOYEE", "CUSTOMER", "DEVELOPER", "TESTER", "USER")
-                .build();
-
-        UserDetails user = User.builder()
-                .username("user")
-                .password("{noop}password")
-                .roles("USER", "EMPLOYEE", "CUSTOMER", "DEVELOPER", "TESTER")
-                .build();
-
-        return new InMemoryUserDetailsManager(admin, user);
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
